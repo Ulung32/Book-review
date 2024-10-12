17b69c860d83f8ea9d23f556883f3edb0a7c74f3
Ulung32 <ulung1208@gmail.com>
2024-10-12 22:10:17 +0700

    init project, migration, and seeder

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Book;
+use App\Models\Review;
+use App\Models\User;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'author' => $this->faker->name,
+            'sumary' => $this->faker->paragraph,
+            'rating' => null,  
+            'reviewCount' => 0,
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Book $book) {
+            $users = User::all();
+
+            // Shuffle users to assign reviews from different users
+            $shuffledUsers = $users->shuffle();
+
+            $reviewCount = rand(3, min($shuffledUsers->count(), 10));
+
+            foreach ($shuffledUsers->take($reviewCount) as $user) {
+                Review::factory()->create([
+                    'book_id' => $book->id,
+                    'user_id' => $user->id,  // Ensure that each review is created by a unique user
+                ]);
+            }
+        
+            $averageRating = $book->reviews()->avg('rating');
+            $reviewCount = $book->reviews()->count();
+
+            $book->update([
+                'rating' => $averageRating,
+                'reviewCount' => $reviewCount,
+            ]);
+        });
+    }
+}
---
+++ database/migrations/2024_10_12_140520_create_reviews_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Foreign key to the users table
+            $table->foreignId('book_id')->constrained()->onDelete('cascade'); // Foreign key to the books table
+            $table->integer('rating');
+            $table->text('review')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
